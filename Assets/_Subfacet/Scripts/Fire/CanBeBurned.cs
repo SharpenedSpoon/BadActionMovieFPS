9eb658d6e28b4127527f4cc027172e51ee5df2e9
Ian Douglas <iandouglas@gmail.com>
2014-01-20 16:04:58 -0700

    Initial fire controller and CanBeBurned components

---
+++ Assets/_Subfacet/Scripts/Fire/CanBeBurned.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CanBeBurned : MonoBehaviour {
+
+	public float fireDamagePerSecond = 0.1f;
+	public bool onFire = false;
+	public float health = 1.0f;
+
+	void Start () {
+		if (! FireController.active) {
+			enabled = false;
+			return;
+		}
+
+		// tell the firecontroller that I exist
+		FireController.active.RegisterBurnableObject(gameObject);
+	}
+
+	void Update() {
+		renderer.material.color = new Color(1.0f - health,1,1);
+	}
+
+	public void HandleFireDamage(float dmg) {
+		if (dmg > 0.0f) {
+			TakeDamage(dmg);
+		} else if (dmg < 0.0f) {
+			HealDamage(dmg);
+		}
+	}
+
+	public void TakeDamage(float dmg) {
+		if (onFire) {
+			return;
+		}
+		health -= dmg;
+		if (health <= 0.0f) {
+			onFire = true;
+		}
+	}
+
+	public void HealDamage(float dmg) {
+		health = Mathf.Min(health + dmg, 1.0f);
+	}
+}
---
+++ Assets/_Subfacet/Scripts/Fire/FireController.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FireController : MonoBehaviour {
+
+	public new static FireController active;
+	public float radius = 1.0f;
+
+	private List<CanBeBurned> burnableObjects = new List<CanBeBurned>();
+	private Dictionary<CanBeBurned, List<CanBeBurned>> burnableNeighbors = new Dictionary<CanBeBurned, List<CanBeBurned>>();
+
+	void Awake() {
+		active = this;
+	}
+
+	void Start() {
+		
+	}
+
+	void Update() {
+		RemoveDestroyedObjects();
+
+		if (burnableObjects.Count > 0) {
+			if ()
+			FigureOutFire();
+		}
+	}
+
+	private void RemoveDestroyedObjects() {
+		burnableObjects.RemoveAll(item => item == null);
+	}
+
+	private void FigureOutFire() {
+		Dictionary<CanBeBurned, float> damageDeltas = new Dictionary<CanBeBurned, float>();
+
+		foreach (CanBeBurned burner in burnableObjects) {
+
+			damageDeltas.Add(burner, 0.0f);
+			foreach (CanBeBurned other in burnableNeighbors[burner]) {
+				if (other.onFire) {
+					damageDeltas[burner] += other.fireDamagePerSecond;
+				} else {
+					damageDeltas[burner] -= 0.5f * other.fireDamagePerSecond;
+				}
+			}
+		}
+
+		foreach (CanBeBurned burner in burnableObjects) {
+			burner.HandleFireDamage(damageDeltas[burner]);
+		}
+	}
+
+	public void RegisterBurnableObject(GameObject go) {
+		CanBeBurned burner = go.GetComponent<CanBeBurned>();
+		if (! burner) {
+			Debug.LogError("Tried to add a burnable object but no CanBeBurned component was found.");
+			return;
+		}
+
+		burnableObjects.Add(burner);
+
+		RegisterAllBurnableNeighbors();
+	}
+	
+	private List<CanBeBurned> FindNeighbors(CanBeBurned go, float radius) {
+		List<CanBeBurned> output = new List<CanBeBurned>();
+		foreach (CanBeBurned burner in burnableObjects) {
+			if (Vector3.Distance(go.gameObject.transform.position, burner.gameObject.transform.position) <= radius) {
+				output.Add(burner);
+			}
+		}
+		return output;
+	}
+
+	private void RegisterAllBurnableNeighbors() {
+		burnableNeighbors = new Dictionary<CanBeBurned, List<CanBeBurned>>();
+		foreach (CanBeBurned burner in burnableObjects) {
+			burnableNeighbors.Add(burner, FindNeighbors(burner, radius));
+		}
+	}
+}

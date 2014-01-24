1dd92e5f0df52a72ac51219768240a28848f5d15
Ian Douglas <iandouglas@gmail.com>
2014-01-24 14:18:22 -0700

    Weapon/bullet public properties and default values

--- Assets/_Subfacet/Scripts/Weapons/_new/Bullet.cs
+++ Assets/_Subfacet/Scripts/Weapons/_new/Bullet.cs
@@ -1,9 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Bullet : MonoBehaviour {
+
+	// TODO convert isLinear to an enum
 	
-	//
+	public bool isHitScan = false;
+	public bool isLinear = true; // otherwise it is a projectile
+	public bool destroyOnHit = true;
+	public float initialSpeed = 10.0f;
+	public float range = 0.0f; // a range of 0 means don't worry about range
+	public float damage = 1.0f;
+	public float lifetime = 20.0f;
+	public List<GameObject> objectsToSpawnOnHit = new List<GameObject>();
+	public List<GameObject> objectsToSpawnOnDestroy = new List<GameObject>();
 	
 	void Start () {
 		
--- Assets/_Subfacet/Scripts/Weapons/_new/Weapon.cs
+++ Assets/_Subfacet/Scripts/Weapons/_new/Weapon.cs
@@ -3,7 +3,17 @@ using System.Collections;
 
 public class Weapon : MonoBehaviour {
 
-	//
+	public GameObject bullet = null;
+	public bool autofire = false;
+	public int ammoCount = 1000;
+	public int magazineSize = 10;
+	public int bulletsPerShot = 1;
+	public float rateOfFire = 30; // shots per second
+	public float reloadTime = 0.5f;
+	public Color gunColor = Color.white;
+	public Vector3 idleHoldObjectPosition = Vector3.zero;
+	public GameObject muzzleFlash = null;
+	public GameObject idleHoldObject = null;
 
 	void Start () {
 	
